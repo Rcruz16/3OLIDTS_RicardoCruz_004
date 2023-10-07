f68987adc497e00061fd103d5164256a6f43cfcd
Ricardo Uriel Crz Crz <ricardo.cruze@outlook.com>
2023-10-07 13:25:43 -0600

    Cambios Version 2

--- 3OLIDTS_RicardoCruz_004/_3OLIDTS_RicardoCruz_004.py
+++ 3OLIDTS_RicardoCruz_004/_3OLIDTS_RicardoCruz_004.py
@@ -1,8 +1,7 @@
-
-#Quitaron los acentos
 import tkinter as tk
 from tkinter import messagebox
-
+import re
+#NO CHAMBEA AQUï¿½, EN SPYDER O PYCHARM SI
 def limpiar_campos():
     entry_nombres.delete(0, tk.END)
     entry_apellidos.delete(0, tk.END)
@@ -12,39 +11,63 @@ def limpiar_campos():
     var_genero.set(0)
 
 def guardar_datos():
-    #Obtener los datos de los campos
+    # Obtener los datos de los campos
     nombres = entry_nombres.get()
     apellidos = entry_apellidos.get()
     edad = entry_edad.get()
     estatura = entry_estatura.get()
     telefono = entry_telefono.get()
-    
+
     genero = ""
     if var_genero.get() == 1:
         genero = "Hombre"
     elif var_genero.get() == 2:
         genero = "Mujer"
-        
-    #Crear una cadena con los datos
-    datos = f"Nombres: {nombres}\nApellidos: {apellidos}\nEdad: {edad}\nGenero: {genero}\nEstatura: {estatura}\nTelefono: {telefono}"
-        
-    # Guardar los datos en un archivo de texto
-    with open("datos99.txt", "a") as archivo:
-        archivo.write(datos + "\n\n")
-        
-    # Mostrar un mensaje con los datos capturados
-    tk.messagebox.showinfo("Informacion", "Datos guardados con exito:\n\n" + datos)
-        
-    limpiar_campos()
 
-#Crear la ventana principal
+    if (es_entero_valido(edad) and es_decimal_valido(estatura) and es_entero_valido_de_10_digitos(telefono)
+        and es_texto_valido(nombres) and es_texto_valido(apellidos)):
+        # Crear una cadena con los datos
+        datos = f"Nombres: {nombres}\nApellidos: {apellidos}\nEdad: {edad}\nGenero: {genero}\nEstatura: {estatura}\nTelefono: {telefono}"
+            
+        # Guardar los datos en un archivo de texto
+        with open("datos99.txt", "a") as archivo:
+            archivo.write(datos + "\n\n")
+            
+        # Mostrar un mensaje con los datos capturados
+        messagebox.showinfo("Informacion", "Datos guardados con exito:\n\n" + datos)
+            
+        limpiar_campos()
+    else:
+        messagebox.showerror("Error", "Por favor, ingrese otros datos vï¿½lidos")
+
+def es_entero_valido(valor):
+    try:
+        int(valor)
+        return True
+    except ValueError:
+        return False
+       
+def es_decimal_valido(valor):
+    try:
+        float(valor)
+        return True
+    except ValueError:
+        return False
+     
+def es_entero_valido_de_10_digitos(valor):
+    return valor.isdigit() and len(valor) == 10
+    
+def es_texto_valido(valor):
+    return bool(re.match("^[a-zA-Z\s]+$", valor))
+
+# Crear la ventana principal
 ventana = tk.Tk()
 ventana.title("Formulario")
 
-#Crear variables para los RadioButtons
+# Crear variables para los RadioButtons
 var_genero = tk.IntVar()
 
-#Crear etiquetas y campos de entrada
+# Crear etiquetas y campos de entrada
 label_nombres = tk.Label(ventana, text="Nombres:")
 label_nombres.pack()
 entry_nombres = tk.Entry(ventana)
@@ -79,12 +102,11 @@ rb_hombre.pack()
 rb_mujer = tk.Radiobutton(ventana, text="Mujer", variable=var_genero, value=2)
 rb_mujer.pack()
 
-
-#Botón para guardar datos
+# Botï¿½n para guardar datos
 btn_guardar = tk.Button(ventana, text="Guardar", command=guardar_datos)
 btn_guardar.pack()
 
-btn_borrar = tk.Button(ventana, text= "Borrar", command=limpiar_campos)
+btn_borrar = tk.Button(ventana, text="Borrar", command=limpiar_campos)
 btn_borrar.pack()
 
 ventana.mainloop()
